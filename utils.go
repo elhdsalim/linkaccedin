beeae7c83e816aa6650d0f5be8a771ecbbcc3d3f
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-02-08 11:58:44 +0100

    set base headers

---
+++ utils.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func SetDefaultHeaders(req *http.Request) {
+	req.Header.Set("Sec-Ch-Ua", `"Not(A:Brand";v="8", "Chromium";v="144"`)
+	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+	req.Header.Set("Sec-Ch-Ua-Platform", `"macOS"`)
+	req.Header.Set("Accept-Language", "fr-FR,fr;q=0.9")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Sec-Fetch-Site", "none")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Priority", "u=0, i")
+	req.Header.Set("Connection", "keep-alive")
+}
