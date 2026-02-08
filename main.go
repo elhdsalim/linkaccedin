373e69ccebda1da334a3c796327c82a2fd1ec4ab
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-02-08 11:59:23 +0100

    make a basic request for /login

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
-
-	tls "github.com/refraction-networking/utls"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
+	req, err := http.NewRequest("GET", Url+Path, nil)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println("utls version : ", tls.HelloChrome_Auto)
+	SetDefaultHeaders(req) // ONLY basic headers that i have with Burp, no cookie ect... (like if it was the first request on a website)
 
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	reader, err := gzip.NewReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
